277de40feeb33c9b37292fac2841a318a89cb51c
Youngrok Lee <cakeil@gmail.com>
2024-01-21 02:31:59 -0500

    add examples 15.1 and 15.2

--- revised/ch15_apriori.R
+++ revised/ch15_apriori.R
@@ -12,6 +12,52 @@ print(transaction_list)
 transactions <- as(transaction_list, "transactions")
 print(transactions)
 
+# ex15.1 - support
+
+# A user defined function to convert a character vector of items to itemMatrix object
+# - itemset: a vector of item labels in a itemset
+# - labels: a vector of all labels ordered to be column order of output matrix
+as_itemMatrix <- function(itemset, transactions) {
+  as(
+    matrix(labels %in% itemset, nrow = 1, dimnames = list(NULL, labels)),
+    "itemMatrix"
+  )
+}
+
+# retrieve item labels used in transactions
+labels <- transactions@itemInfo$labels
+
+# create itemMatrix object for X = {b, c}, and compute the support
+X <- as_itemMatrix(c("b", "c"), labels)
+supp_X <- support(X, transactions)
+print(supp_X)
+
+# create itemMatrix object for Y = {g}, and compute the support
+Y <- as_itemMatrix(c("g"), labels)
+
+# create a rule R: X -> Y, and compute the support
+R <- rules(Y, X)
+supp_R <- support(R, transactions)
+print(supp_R)
+
+# ex15.2 - confidence and lift
+
+# confidence
+conf_R <- supp_R / supp_X
+print(conf_R)
+# when using a function instead:
+interestMeasure(R, "confidence", transactions)
+
+
+# lift
+supp_Y <- support(Y, transactions)
+lift_R <- conf_R / supp_Y
+print(lift_R)
+# when using a function instead:
+interestMeasure(R, "lift", transactions)
+
+
+
 # ex15.3 - generate frequent itemsets with minimal support 0.4
 itemsets <- apriori(
   transactions,

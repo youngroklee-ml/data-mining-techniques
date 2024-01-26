40c258801a2d6cc229945b0d17b0074d96223478
Youngrok Lee <cakeil@gmail.com>
2024-01-25 19:06:03 -0500

    add R code for collaborative filtering

---
+++ original/ch16_collaborative_filtering.R
@@ -0,0 +1,36 @@
+# ex16.3
+
+# load library
+library("recommenderlab")
+
+# load data
+df <- read.csv("data/ch16_ratings.csv")
+
+# convert to rating matrix
+mat <- sparseMatrix(i = df$customer, j = df$item, x = df$rating)
+ratings <- as(mat, "realRatingMatrix")
+ratings
+
+# set target customer to generate recommendation
+target <- 7
+
+# compute similarity
+centered <- normalize(ratings)
+centered
+similarity(centered[-target], centered[target])
+
+# train recommender engine with all users
+rec <- Recommender(ratings, method = "UBCF")
+rec
+
+# predict ratings from target user
+predicted <- predict(rec, target, type = "ratings")
+predicted
+print(predicted@data)
+
+# create top-N recommendations for target user
+recommended <- predict(rec, target, n = 2)
+print(recommended)
+print(recommended@items)
+print(recommended@ratings)
+

3a37f9b8f7969ee123cacd1176ccbed1c7ab2f45
Youngrok Lee <cakeil@gmail.com>
2024-02-18 10:57:43 -0500

    rename chap7 files

--- revised/ch07_ex06_QDA.R
+++ revised/ch07_ex06_QDA.R
@@ -1,13 +1,11 @@
-# ch7_LDA.R
-# ch7.2 Linear Discriminant Analysis
-
-# ex7.4
+# ch07_ex06_QDA.R
+# ch7.4 Quadratic Discriminant Analysis
 
 # load library
-library(MASS) # for LDA
-library(yardstick) # performance measurement
+library(MASS) # QDA
+library(yardstick) # measure performance
 
-# read csv file
+# read csv file# read csv file
 dat1 <- read.csv("data/ch7_dat1.csv")
 
 # remove ID variable
@@ -16,13 +14,14 @@ dat1 <- dat1[, -1]
 # define class(1,2) as a factor variable
 dat1$class <- as.factor(dat1$class)
 
-# Linear Discriminant Analysis (LDA)
-lda_fit <- lda(class ~ ., data = dat1, prior = c(1 / 2, 1 / 2))
-lda_fit
+# ex7.6
+# Quadratic Discriminant Analysis (QDA)
+qda_fit <- qda(class ~ ., data = dat1, prior = c(1 / 2, 1 / 2))
+qda_fit
 
 # predict test data set:
 # in this example, test data is the same to training data
-pred <- predict(lda_fit, dat1)
+pred <- predict(qda_fit, dat1)
 
 # predicted posterior probabilities
 pred$posterior
@@ -34,6 +33,6 @@ pred$class
 results <- cbind(dat1, posterior = pred$posterior, pred_class = pred$class)
 results
 
-# measuring performance of LDA
+# measuring performance of QDA
 metrics_multi <- metric_set(accuracy, sens, spec, f_meas, roc_auc)
 metrics_multi(results, truth = "class", estimate = "pred_class", posterior.1)

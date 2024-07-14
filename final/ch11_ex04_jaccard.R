d2fdd30f8351dd89b99db120ceecf1e5bafeade5
Youngrok Lee <cakeil@gmail.com>
2024-07-13 23:16:54 -0400

    fix jaccard similarity calculation

--- final/ch11_ex04_jaccard.R
+++ final/ch11_ex04_jaccard.R
@@ -9,7 +9,7 @@ df <- read.csv("data/ch11_dat2.csv")
 # a: number of variables valued 1 in both x and y
 # b: number of variables valued 1 in x, but 0 in y
 # c: number of variables valued 0 in x, but 1 in y
-jaccard_sim <- dist(df, method = "binary")
+jaccard_sim <- 1 - dist(df, method = "binary")
 jaccard_sim
 jaccard_sim_mat <- as.matrix(jaccard_sim)
 
